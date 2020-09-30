baa54d0f0a9f1244fa6a367028e97b926572823b
NIMO <laomms@gmail.com>
2020-09-30 08:52:35 +0700

    添加项目文件。

---
+++ CallbackFromCshap/CallbackFromCshap.cpp
@@ -0,0 +1,36 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+typedef bool(*FuncTest)(const char* data, const char* data2);
+typedef bool(*functionPointer)(const char* data, const char* data2);
+typedef bool(*CallbackFunction)(functionPointer callback);
+
+
+bool realFunction(const char* data1, const char* data2)
+{
+    std::cout << data1  << "\n";
+    std::cout << data2 << "\n";
+    return true;
+}
+
+
+int main()
+{
+    HMODULE hDll = LoadLibrary(L".\\ManagedDll.dll");
+    if (hDll != NULL)
+    {
+        CallbackFunction DLLFuncPtr = (CallbackFunction)GetProcAddress(hDll, "CallbackFunction");
+        if (DLLFuncPtr != NULL)
+        {
+            DLLFuncPtr(&realFunction);
+        }
+
+        FuncTest pFuncTest = (FuncTest)GetProcAddress(hDll, "FuncTest");
+        if (pFuncTest != NULL)
+        {
+            pFuncTest("测试数据1", "测试数据2");
+        }
+    }
+    system("pause");
+    return 0;
+}
---
+++ ManagedDll/Class1.cs
@@ -0,0 +1,34 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedDll
+{
+    public class Class1
+    {
+        public delegate bool DelegateFunc(string data1, string data2);
+
+        public static DelegateFunc GetResult = null;
+        [DllExport("CallbackFunction", CallingConvention = CallingConvention.Cdecl)]
+        public static bool CallbackFunction(IntPtr callback)
+        {
+            DelegateFunc myFunc = (DelegateFunc)Marshal.GetDelegateForFunctionPointer(callback, typeof(DelegateFunc));
+            GetResult = myFunc;
+            return true;
+        }
+
+
+        
+        [DllExport("FuncTest", CallingConvention = CallingConvention.Cdecl)]
+        public static bool FuncTest(string data1, string data2)
+        {
+            GetResult(data2,data1);
+            return true;
+        }
+
+    }
+}
